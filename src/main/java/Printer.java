9d924954319590c65e5e6f3f56320eb712270be8
leeburgess67 <leeburgessmail@gmail.com>
2018-06-25 16:19:19 +0100

    printer created

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,8 @@
 public class Printer {
+    private int sheets;
+
+    public Printer(int sheets) {this.sheets = sheets;}
+
+    public int getSheets() {return this.sheets;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,3 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
-    private int sheetsLeft
+     Printer printer;
+
+     @Before
+     public void before() {
+          printer = new Printer(100);
+     }
+
+     @Test
+     public void hasSheets() {
+          assertEquals(100, printer.getSheets());
+     }
+
 }
